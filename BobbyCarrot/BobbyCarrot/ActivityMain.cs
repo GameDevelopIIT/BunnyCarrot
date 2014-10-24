480ebf21ac535fc977aa1578ebffffc1b924e0b7
Danil <yusupdan@gmail.com>
2014-10-24 20:57:13 +0600

    Initial comit

---
+++ BobbyCarrot/BobbyCarrot/ActivityMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.Xna.Framework;
+
+namespace BobbyCarrot
+{
+	[Activity (Label = "BobbyCarrot", 
+		MainLauncher = true,
+		Icon = "@drawable/icon",
+		Theme = "@style/Theme.Splash",
+		AlwaysRetainTaskState = true,
+		LaunchMode = Android.Content.PM.LaunchMode.SingleInstance,
+		ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation |
+		Android.Content.PM.ConfigChanges.KeyboardHidden |
+		Android.Content.PM.ConfigChanges.Keyboard)]
+	public class Activity1 : AndroidGameActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create our OpenGL view, and display it
+			Game.Activity = this;
+			var g = new Game ();
+			SetContentView (g.Window);
+			g.Run ();
+		}
+		
+	}
+}
+
+
