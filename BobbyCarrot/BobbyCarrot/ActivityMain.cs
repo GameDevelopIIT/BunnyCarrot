e5902d0a6a046a3187944940fc57b6449f367888
Danil <yusupdan@gmail.com>
2014-10-25 00:08:20 +0600

    reformat code

--- BobbyCarrot/BobbyCarrot/ActivityMain.cs
+++ BobbyCarrot/BobbyCarrot/ActivityMain.cs
@@ -11,29 +11,28 @@ using Microsoft.Xna.Framework;
 
 namespace BobbyCarrot
 {
-	[Activity (Label = "BobbyCarrot", 
-		MainLauncher = true,
-		Icon = "@drawable/icon",
-		Theme = "@style/Theme.Splash",
-		AlwaysRetainTaskState = true,
-		LaunchMode = Android.Content.PM.LaunchMode.SingleInstance,
-		ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation |
-		Android.Content.PM.ConfigChanges.KeyboardHidden |
-		Android.Content.PM.ConfigChanges.Keyboard)]
-	public class Activity1 : AndroidGameActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    [Activity (Label = "BobbyCarrot", 
+        MainLauncher = true,
+        Icon = "@drawable/icon",
+        Theme = "@style/Theme.Splash",
+        AlwaysRetainTaskState = true,
+        LaunchMode = Android.Content.PM.LaunchMode.SingleInstance,
+        ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation |
+                               Android.Content.PM.ConfigChanges.KeyboardHidden |
+                               Android.Content.PM.ConfigChanges.Keyboard)]
+    public class Activity1 : AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Create our OpenGL view, and display it
-			Game.Activity = this;
-			var g = new Game ();
-			SetContentView (g.Window);
-			g.Run ();
-		}
-		
-	}
+            // Create our OpenGL view, and display it
+            Game.Activity = this;
+            var g = new Game();
+            SetContentView(g.Window);
+            g.Run();
+        }
+    }
 }
 
 
--- BobbyCarrot/BobbyCarrot/Game.cs
+++ BobbyCarrot/BobbyCarrot/Game.cs
@@ -1,6 +1,6 @@
 ﻿#region Using Statements
-using System;
 
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
@@ -10,81 +10,81 @@ using Microsoft.Xna.Framework.Input;
 
 namespace BobbyCarrot
 {
-	/// <summary>
-	/// This is the main type for your game
-	/// </summary>
-	public class BobbyCarrotGame : Game
-	{
-		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
-		Texture2D logoTexture;
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class BobbyCarrotGame : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Texture2D logoTexture;
 
-		public BobbyCarrotGame ()
-		{
-			graphics = new GraphicsDeviceManager (this);
-			Content.RootDirectory = "Content";	            
-			graphics.IsFullScreen = true;		
-		}
+        public BobbyCarrotGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.IsFullScreen = true;
+        }
 
-		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
-		/// </summary>
-		protected override void Initialize ()
-		{
-			// TODO: Add your initialization logic here
-			base.Initialize ();
-				
-		}
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
 
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
-		protected override void LoadContent ()
-		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch (GraphicsDevice);
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			//TODO: use this.Content to load your game content here 
-			logoTexture = Content.Load<Texture2D> ("logo");
-		}
+            //TODO: use this.Content to load your game content here 
+            logoTexture = Content.Load<Texture2D>("logo");
+        }
 
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Update (GameTime gameTime)
-		{
-			// For Mobile devices, this logic will close the Game when the Back button is pressed
-			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
-				Exit ();
-			}
-			// TODO: Add your update logic here			
-			base.Update (gameTime);
-		}
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
+                Exit();
+            }
+            // TODO: Add your update logic here			
+            base.Update(gameTime);
+        }
 
-		/// <summary>
-		/// This is called when the game should draw itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Draw (GameTime gameTime)
-		{
-			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
-		
-			//TODO: Add your drawing code here
-			spriteBatch.Begin ();
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			// draw the logo
-			spriteBatch.Draw (logoTexture, new Vector2 (40, 40), Color.White);
+            //TODO: Add your drawing code here
+            spriteBatch.Begin();
 
-			spriteBatch.End ();
+            // draw the logo
+            spriteBatch.Draw(logoTexture, new Vector2(40, 40), Color.White);
 
-			base.Draw (gameTime);
-		}
-	}
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
 }
 
