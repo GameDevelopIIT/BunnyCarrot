033f509b7ca73234b4c5cf9aa0cca492bc172127
Алексей Кочев <kochev.alexey@gmail.com>
2014-11-03 12:47:50 +0500

    +some code

--- BunnyCarrot.Android/BunnyCarrot.Android/MainActivity.cs
+++ BunnyCarrot.Android/BunnyCarrot.Android/MainActivity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -7,40 +6,36 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Content.PM;
+using Microsoft.Xna.Framework;
+using CocosSharp;
 
 namespace BunnyCarrot.Android
 {
-	[Activity(
-		Label = "BouncingGame",
-		AlwaysRetainTaskState = true,
-		Icon = "@drawable/icon",
-		Theme = "@android:style/Theme.NoTitleBar",
-		LaunchMode = LaunchMode.SingleInstance,
-		ScreenOrientation = ScreenOrientation.Portrait,
-		MainLauncher = true,
-		ConfigurationChanges =  ConfigChanges.Keyboard | 
-		ConfigChanges.KeyboardHidden)
-	]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(
+        Label = "Bunny Carrot",
+        AlwaysRetainTaskState = true,
+        Icon = "@drawable/icon",
+        Theme = "@android:style/Theme.NoTitleBar",
+        LaunchMode = LaunchMode.SingleInstance,
+        ScreenOrientation = ScreenOrientation.Landscape,
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.Keyboard |
+        ConfigChanges.KeyboardHidden)
+    ]
+    public class MainActivity : AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+            var application = new CCApplication();
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+            // GameAppDelegate is your class that inherits from CCApplicationDelegate
+            application.ApplicationDelegate = new BunnyCarrot.UI.AppDelegate();
+            SetContentView(application.AndroidContentView);
+            application.StartGame();
+        }
+    }
 }
 
 
---
+++ BunnyCarrot.Android/BunnyCarrot/UI/GameAppDelegate.cs
@@ -0,0 +1,41 @@
+﻿using CocosDenshion;
+using CocosSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyCarrot.UI
+{
+    public class AppDelegate : CCApplicationDelegate
+    {
+        public override void ApplicationDidFinishLaunching(CCApplication application, CCWindow mainWindow)
+        {
+            application.PreferMultiSampling = false;
+            application.ContentRootDirectory = "Content";
+            //mainWindow.SupportedDisplayOrientations = CCDisplayOrientation.Default;
+
+            // Установим общее разрешение, котораое будет использоваться для всех сцен
+            Resolution.DesignResolution = mainWindow.DesignResolutionSize;
+
+            // todo:  Add our GameScene initialization here
+        }
+
+        public override void ApplicationDidEnterBackground(CCApplication application)
+        {
+            // Stop all of the animation actions that are running.
+            application.Paused = true;
+
+            // If SimpleAudioEngine is used, the music must be paused.
+            CCSimpleAudioEngine.SharedEngine.PauseBackgroundMusic();
+        }
+
+        public override void ApplicationWillEnterForeground(CCApplication application)
+        {
+            application.Paused = false;
+            CCSimpleAudioEngine.SharedEngine.ResumeBackgroundMusic();
+        }
+    }
+
+}
